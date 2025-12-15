80de5ab5b3b6ca1454ca04b10c18309674b3321b
Ahmad Arief <ahmadarief4701@gmail.com>
2025-12-15 13:53:39 +0800

    Wireless Capabilities

--- run_scrcpy_wireless.bat
+++ run_scrcpy_wireless.bat
@@ -7,6 +7,9 @@ set ADB=%~dp0platform-tools\adb.exe
 REM Set the path to the scrcpy server
 set SCRCPY_SERVER_PATH=%~dp0x\server\scrcpy-server
 
+REM Set the path to the scrcpy icon
+set SCRCPY_ICON_PATH=%~dp0app\data\icon.png
+
 REM Load last IP
 set "LAST_IP="
 if exist "%~dp0scrcpy-last-ip.txt" (
